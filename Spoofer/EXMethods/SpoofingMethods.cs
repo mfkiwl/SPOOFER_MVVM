2b8387ec4942d488a5a42c5e426efb4f8c1d5d8c
Max Parker <prkrmx@gmail.com>
2022-03-23 12:29:09 +0200

    initial

---
+++ Spoofer/EXMethods/SpoofingMethods.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spoofer.EXMethods
+{
+    public static class SpoofingMethods
+    {
+        private const string LIBRARY_FILENAME = "";
+       
+    }
+}
