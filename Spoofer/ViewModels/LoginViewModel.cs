aad691f6a1ba69385bc67f0897bf7ade6615a63b
Max Parker <prkrmx@gmail.com>
2022-07-05 16:03:48 +0300

    Initial

--- Spoofer/ViewModels/LoginViewModel.cs
+++ Spoofer/ViewModels/LoginViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using Spoofer.Commands.UserCommands;
 using Spoofer.Services.User;
 using Spoofer.Services.User.Register;
+using System;
+using System.Reflection;
 using System.Windows.Input;
 
 namespace Spoofer.ViewModels
@@ -12,12 +14,16 @@ namespace Spoofer.ViewModels
 
         public LoginViewModel(ILogin iLogin, IRegister register)
         {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            Version version = assembly.GetName().Version;
+            Description = $"Software Version  {version}\nCopyright © 1996-{DateTime.Now.ToString("yyyy")}";
             _iLogin = iLogin;
             _register = register;
             ErrorMessageViewModel = new MessageViewModel();
             Login = new LoginCommand(_iLogin, this);
             Register = new RegisterCommand(_register, this);
         }
+        public string Description { get; set; }
 
         private string password;
 
